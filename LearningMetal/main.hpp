26a572eabea77bba51334e696cf9d0f2e6b1bd7c
eternal <100830314+orientlegend@users.noreply.github.com>
2024-04-14 16:36:19 +0800

    update: separate Renderer into an another file

--- LearningMetal/main.hpp
+++ LearningMetal/main.hpp
@@ -8,40 +8,12 @@
 #ifndef main_h
 #define main_h
 
-#define NS_PRIVATE_IMPLEMENTATION
-#define UI_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
-#define MTL_PRIVATE_IMPLEMENTATION
-#define MTK_PRIVATE_IMPLEMENTATION
+
 #include <Metal/Metal.hpp>
 #include <MetalKit/MetalKit.hpp>
 #include <UIKit/UIKit.hpp>
-
 #include <simd/simd.h>
-
-class Renderer {
-public:
-    Renderer(MTL::Device* pDevice);
-    ~Renderer();
-    void buildShaders();
-    void buildBuffers();
-    void buildFrameData();
-    void draw(MTK::View* pView);
-    
-private:
-    MTL::Device* _pDevice;
-    MTL::CommandQueue* _pCommandQueue;
-    MTL::Library* _pShaderLibrary;
-    MTL::RenderPipelineState* _pPSO;
-    MTL::Buffer* _pArgBuffer;
-    MTL::Buffer* _pVertexPositionsBuffer;
-    MTL::Buffer* _pVertexColorsBuffer;
-    MTL::Buffer* _pFrameData[3];
-    float _angle;
-    int _frame;
-    dispatch_semaphore_t _semaphore;
-    static const int kMaxFramesInFlight;
-};
+#include "Renderer.hpp"
 
 class MyMTKViewDelegate : public MTK::ViewDelegate {
 public:
@@ -67,10 +39,6 @@ private:
     MTK::View* _pMtkView;
     MTL::Device* _pDevice;
     MyMTKViewDelegate* _pViewDelegate = nullptr;
-};
-
-struct FrameData {
-    float angle;
 };
 
 #endif /* main_h */
